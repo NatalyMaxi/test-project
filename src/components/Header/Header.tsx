7ff48dfe142b8a427636acbdad3d240032d22d3f
Natalya Maximova <natka-31@mail.ru>
2025-07-11 23:49:10 +0500

    feat: add a header background change depending on the theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useContext } from 'react';
+import { ThemeContext } from '@/context/theme-provider';
 import { Logo, Navigation, ThemeSwitcher } from '@/components';
 
 export const Header = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <header className="bg-gray-600 text-white">
+    <header className={`${theme === 'dark' ? 'bg-teal-800' : 'bg-gray-600'} text-white`}>
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
         <Logo />
         <div className="flex items-center space-x-4">
